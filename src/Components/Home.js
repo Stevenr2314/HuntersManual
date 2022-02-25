652fdd046f6f4d892fe6042e13c1ac56ff6d89c6
Steven Rosa <s.rosa21@icloud.com>
2022-02-25 08:52:15 -0500

    first commit

---
+++ src/Components/Home.js
@@ -0,0 +1,10 @@
+import React, {useEffect, useState} from "react";
+
+export default function Home (){
+
+    return (
+        <div>
+            <h1>Home Page</h1>
+        </div>
+    )
+}
---
+++ src/Components/Navbar/index.js
@@ -0,0 +1,9 @@
+import React, {useEffect, useState} from "react";
+
+export default function Navbar () {
+    return (
+        <div>
+            <h1>Navbar</h1>
+        </div>
+    )
+}
